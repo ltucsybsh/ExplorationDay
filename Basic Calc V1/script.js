63106cb8aaa8e6a883d2289486ee836272b59cb2
Dr Yashar Baradaranshokouhi <91670386+ltucsybsh@users.noreply.github.com>
2024-05-13 11:02:08 +0100

    Files Uploaded for Calc Activity

---
+++ Basic Calc V1/script.js
@@ -0,0 +1,24 @@
+let display = document.getElementById('display');
+
+function inputValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+        setTimeout(() => {
+            clearDisplay();
+        }, 1500);
+    }
+}
